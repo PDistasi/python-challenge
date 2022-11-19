3bd12c6e1ce611c916e7d42428a4fd15a1c0b392
PDistasi <112498067+PDistasi@users.noreply.github.com>
2022-11-19 15:50:31 -0500

    Corrected all errors and updated code

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -9,64 +9,65 @@ import csv
 import statistics
 
 #Read in .csv file
-budgetcsv = os.path.join("PyBank","Resources","budget_data.csv")
-
-#Set output to text file
-text = os.path.join("analysis","financial.analysis.txt")
+file = os.path.join("PyBank","Resources", "budget_data.csv")
 
 #create lists to store data
 profits = []
 changes = []
+date = []
 
 #Set counters to 0
 month = 0
 profit = 0
 total_change = 0
-total_profit = 0
 greatest_increase = 0
 greatest_decrease = 0
-#Set max profit and losses as empty
-top_profit = ''
-top_loss = ''
-
-
 
 #Open source file and encode
-with open(budgetcsv) as csvfile:
+with open(file) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
-    #skip the header line in file
+#Skip the header line in file
     next(csvreader)
     
-    #Begin looping through and analyzing data
+#Begin looping through and analyzing data
     for row in csvreader:
         
-        #The total number of months included in the dataset
+#The total number of months included in the dataset
         month += 1
         
-        #The net total amount of "Profit/Losses" over the entire period
+#Populate date list for later use
+        date.append(row[0])
         
+#The net total amount of "Profit/Losses" over the entire period
         profit += int(row[1])
         
-        #If statement to calculate top profit and loss
-        #The greatest increase in profits (date and amount) over the entire period
+#If statement to calculate top profit and loss
+#The greatest increase in profits (date and amount) over the entire period
         if int(row[1]) > greatest_increase:
             top_profit = (row[0])
             greatest_increase = int(row[1])
         
-#         #The greatest decrease in profits (date and amount) over the entire period
+#The greatest decrease in profits (date and amount) over the entire period
         elif int(row[1]) < greatest_decrease:
             top_loss = (row[0])
             greatest_decrease = int(row[1])
         changes.append(int(row[1]))
 
-# #calculate monthly changes
+#Calculate monthly changes (dollar amount)
 for i in range(len(changes)-1):
     total_change = (changes[i+1] - changes[i])
     profits.append(total_change)
+    
+#Assign variables for display
+max_profits = max(profits)
+min_profits = min(profits)
 
+max_date = date[profits.index(max_profits)+1]
+min_date = date[profits.index(min_profits)+1]
+    
 # #calculate average profits    
-average_change = statistics.mean(profits)
+average_change = round(statistics.mean(profits),2)
 
 # #print data
 print("Financial Analysis")
@@ -74,8 +75,8 @@ print("----------------------------")
 print(f"Total Months: {month}")
 print(f"Total: ${profit}")
 print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {top_profit} (${greatest_increase})")
-print(f"Greatest Decrease in Profits: {top_loss} (${greatest_decrease})")
+print(f"Greatest Increase in Profits: {(max_date)} (${max_profits})")
+print(f"Greatest Decrease in Profits: {(min_date)} (${min_profits})")
 
 # #Create .txt file with same results
 f = open("financial_analysis.txt", "w")
@@ -84,6 +85,6 @@ f.write("----------------------------\n")
 f.write(f"Total Months: {month}\n")
 f.write(f"Total: ${profit}\n")
 f.write(f"Average Change: ${average_change}\n")
-f.write(f"Greatest Increase in Profits: {top_profit} (${greatest_increase})\n")
-f.write(f"Greatest Decrease in Profits: {top_loss} (${greatest_decrease})\n")
+f.write(f"Greatest Increase in Profits: {max_date} (${max(profits)})\n")
+f.write(f"Greatest Decrease in Profits: {min_date} (${min(profits)})\n")
 
